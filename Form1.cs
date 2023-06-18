0237d20fdd364ad1a03200ac9305acac877beaf2
Nikita Sychev <nikitasychev@inbox.ru>
2023-06-18 10:03:43 +0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int n1;
+        public int n2;
+        public string action;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            action = button.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            action = button.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox2.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int n2 = int.Parse(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            action = button.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            action = button.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            action = button.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            int result = 0;
+            if (action == "+")
+            {
+                result = n1 + n2;
+            }
+            else if (action == "-")
+            {
+                result = n1 - n2;
+            }
+            else if (action == "*")
+            {
+                result = n1 * n2;
+            }
+            else if (action == "/")
+            {
+                result = n1 / n2;
+            }
+            if (action == "=")
+            {
+                textBox3.Text = result.ToString();
+            }
+            
+            
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(textBox2.Text);
+
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        //    Console.WriteLine("Введите число 1:");
+        //    int num1 = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Введите число 2:");
+        //    int num2 = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Выберете действие: + = 1, - = 2, * = 3, / = 4");
+        //    int choise = int.Parse(Console.ReadLine());
+        //    int result;
+        //    string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin", "Debug");
+        //    do
+        //    {
+        //        switch (choise)
+        //        {
+        //            case 1: result = num1 + num2;
+        //                File.WriteAllText( path, result.ToString()); break;
+        //            case 2: result = num1 - num2;
+        //                File.WriteAllText( path, result.ToString()); break;
+        //            case 3: result = num1 * num2;
+        //                File.WriteAllText( path, result.ToString()); break;
+        //            case 4: result = num1 / num2;
+        //                File.WriteAllText( path, result.ToString()); break;
+
+        //        }
+                
+        //    } while (choise > 0 && choise < 5);
+            
+        }
+    }
+}
